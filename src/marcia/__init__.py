0f441e13ebfb12214613821b9922cc29402dea91
Hadrien Meyer <meyerhadrien96@gmail.com>
2021-09-13 21:42:38 +0200

    Remove numpy deprecation warning

--- src/marcia/__init__.py
+++ src/marcia/__init__.py
@@ -1,8 +1,12 @@
+import warnings
+
 import matplotlib as mpl
 
 from .core import *
 from .fitting import *
 from .io import *
 from .plotting import *
+
+warnings.filterwarnings("ignore", category=np.VisibleDeprecationWarning)
 
 mpl.rcParams['image.cmap'] = 'cividis'
